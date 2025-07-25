2559113c25f838593f02250637ec4db3daa9515f
Tamim-Rahman101 <tamim101rahman@gmail.com>
2025-07-25 23:16:33 +0600

    Encapsulation Example Demonstrated

---
+++ Assignment/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class BankAccount
+    {
+        private double Balance;
+
+        public BankAccount()
+        {
+            Balance = 0;
+            Console.WriteLine("New account created with initial balance " + Balance);
+        }
+        public void Deposite(double amount)
+        {
+            if(amount > 0)
+            {
+                Balance += amount;
+                Console.WriteLine($"{amount} is deposited. New balance is {Balance}");
+            }
+        }
+        public void Withdraw(double amount)
+        {
+            if(Balance -  amount >= 0)
+            {
+                Balance -= amount;
+                Console.WriteLine($"{amount} is withdrawn. New balance is {Balance}");
+            }
+        }
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using Assignment;
+
+Console.WriteLine("Encapsulation Example:");
+BankAccount account = new BankAccount();
+account.Deposite(123.5);
+account.Withdraw(120);
+
