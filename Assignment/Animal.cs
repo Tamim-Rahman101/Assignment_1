d4b770f2ca9869623d1765bf6f8f48b6ccba35ce
Tamim-Rahman101 <tamim101rahman@gmail.com>
2025-07-26 00:46:49 +0600

    Inheritance Example Demonstrated

---
+++ Assignment/Animal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("New animal created.");
+        }
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating.");
+        }
+    }
+    internal class Dog : Animal
+    {
+        public Dog()
+        {
+            Console.WriteLine("New dog created.");
+        }
+        public void Speak()
+        {
+            Console.WriteLine("Dog speaks bow bow.");
+        }
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,4 +4,10 @@ Console.WriteLine("Encapsulation Example:");
 BankAccount account = new BankAccount();
 account.Deposite(123.5);
 account.Withdraw(120);
+Console.WriteLine();
 
+Console.WriteLine("Inheritance Example:");
+Dog dog = new Dog();
+dog.Eat();
+dog.Speak();
+Console.WriteLine();
